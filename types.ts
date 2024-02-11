e1b24cb28aa5bd2df5b3b220d941b17a2ff674bb
Şeyma <100444600+seymanurks@users.noreply.github.com>
2024-02-11 18:49:10 +0300

    types

---
+++ types.ts
@@ -0,0 +1,9 @@
+let a: number = 5;
+let b: string = 'a';
+let c: boolean = true;
+let d: any;
+let e: number [] = [1, 2, 3];
+let f: Array<number> = [1, 2, 3];
+let g: any[] = [1, 'a', true];
+let h: [string, number, boolean] = ['a', 5, false]; //tuple
+
